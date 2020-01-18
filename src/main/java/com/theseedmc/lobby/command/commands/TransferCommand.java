e1325748d7455628b0bfc67a92e64665c5c7c16d
Graham <grahamphiliarmonica@gmail.com>
2020-01-18 01:43:43 -0600

    Update TransferCommand.java

--- src/main/java/com/theseedmc/lobby/command/commands/TransferCommand.java
+++ src/main/java/com/theseedmc/lobby/command/commands/TransferCommand.java
@@ -17,7 +17,7 @@ public class TransferCommand extends AbstractCommand {
         Player player = (Player) sender;
 
         if (args.length != 1 || args[0] == null) {
-            player.sendMessage(Utils.replaceColors("&c&lERROR: &7Usage: /transfer <melon|pumpkin|mushroom|lobby-[1..3]>"));
+            player.sendMessage(Utils.replaceColors("&c&lERROR: &7Usage: /transfer <melon|pumpkin|mushroom>"));
             return;
         }
 
